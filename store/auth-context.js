c275f3114b674f6b94152dbc87a51c132f82be97
Zachary Delcoco <zdelcoco@dmsi.com>
2024-08-05 16:45:57 -0500

    lecture 181 - storing & managing auth state

---
+++ store/auth-context.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext({
+  token: '',
+  isAuthenticated: false,
+  authenticate: () => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
+
+  function authenticate(token) {
+    setAuthToken(token);
+  }
+
+  function logout() {
+    setAuthToken(null);
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value} r>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContextProvider;
