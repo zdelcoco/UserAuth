324d9ac35f9ebca41128f9e069082d3ecff1b01e
Zachary Delcoco <zdelcoco@dmsi.com>
2024-08-05 16:16:57 -0500

    lecture 178 - creating new users

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyDTbVw2_RZ8YwVol0m41UHxtu0rrfeXYoI';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
